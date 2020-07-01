9dbaeb76d8300159fc503276d99a55dd67147e7e
Andrew <atsmith3@illinois.edu>
2020-07-01 14:34:30 -0500

    Bugfix on printing the accuracy

--- run_perceptron.sh
+++ run_perceptron.sh
@@ -4,6 +4,6 @@
   --input_csv_train='./data-cleaning/optdigits-orig_train.csv' \
   --input_csv_test='./data-cleaning/optdigits-orig_test.csv' \
   --events=1024 \
-  --epochs=1000 \
+  --epochs=64 \
   --actions=10 \
   --eta=0.25
